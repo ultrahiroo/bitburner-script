af52b2f8a14c4084dcbbcccbf5c5566ac4f7670e
Inoue Hiroo <ultrahiroo2000@yahoo.co.jp>
2025-11-26 03:17:43 +0900

    WIP

---
+++ config.mjs
@@ -0,0 +1,33 @@
+import { context } from 'esbuild';
+import { BitburnerPlugin } from 'esbuild-bitburner-plugin';
+
+const createContext = async () => await context({
+  entryPoints: [
+    'servers/**/*.js',
+    'servers/**/*.jsx',
+    'servers/**/*.ts',
+    'servers/**/*.tsx',
+  ],
+  outbase: "./servers",
+  outdir: "./build",
+  plugins: [
+    BitburnerPlugin({
+      port: 12525,
+      types: 'NetscriptDefinitions.d.ts',
+      distribute: {
+        'build/home/dist': ['n00dles'],
+      },
+      mirror: {
+        'mirror': ['home'],
+      },
+      pushOnConnect: true,
+    })
+  ],
+  bundle: true,
+  format: 'esm',
+  platform: 'browser',
+  logLevel: 'debug',
+});
+
+const ctx = await createContext();
+ctx.watch();
